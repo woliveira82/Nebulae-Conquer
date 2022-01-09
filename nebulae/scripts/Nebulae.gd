4ae203b97f8edc681a9c74325ea41eb1be3809de
Weiner Oliveira <woliveira82@gmail.com>
2022-01-08 22:36:12 -0300

    fix: drag and draw

--- nebulae/scripts/Nebulae.gd
+++ nebulae/scripts/Nebulae.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 var line_from = Vector2.ZERO
 var line_to = Vector2.ZERO
+var origin_planet
+var destiny_planet
 var dragging = false
 export var snap_distance = 50
+enum {
+	PRESS,
+	DRAG,
+	RELEASE
+}
 
 onready var planet_list = $Planets.get_children()
 
@@ -15,7 +22,7 @@ func _process(delta):
 
 
 func _draw():
-	if dragging:
+	if dragging and line_from:
 		draw_line(line_from, line_to, Color(1.0, 1.0, 1.0, 0.4), 4, true)
 
 
@@ -23,17 +30,34 @@ func _input(event):
 	if event is InputEventScreenTouch:
 		dragging = event.pressed
 		if event.pressed:
-			line_from = _snap_to_planet(event.position)
+			line_from = _snap_to_planet(event.position, PRESS)
 			line_to = event.position
 		else:
-			line_to = _snap_to_planet(event.position)
+			line_to = _snap_to_planet(event.position, RELEASE)
 	else:
 		if dragging:
-			line_to = _snap_to_planet(event.position)
+			line_to = _snap_to_planet(event.position, DRAG)
 
 
-func _snap_to_planet(position):
+func _snap_to_planet(position, action):
 	for planet in planet_list:
 		if position.distance_to(planet.global_position) < snap_distance:
-			return planet.global_position
+			if action == PRESS:
+				origin_planet = planet
+				destiny_planet = null
+				return planet.global_position
+			elif action == DRAG:
+				return planet.global_position
+			else:
+				destiny_planet = planet
+				if origin_planet != destiny_planet and origin_planet != null:
+					_create_attack_fleet(origin_planet, destiny_planet)
+				return planet.global_position
+	if action == PRESS:
+		origin_planet = null
+		return null
 	return position
+
+
+func _create_attack_fleet(origin, destiny):
+	print('attack')
